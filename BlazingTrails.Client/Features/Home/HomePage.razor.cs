e49d30733b5cd69a707272a892b65e1c471a2caa
Abdurabd <49178533+Abdurabd@users.noreply.github.com>
2023-02-22 12:01:15 +0000

    Covered until component life cycle but now improvement as the basics where learnt once again

---
+++ BlazingTrails.Client/Features/Home/HomePage.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace BlazingTrails.Client.Features.Home
+{
+    public partial class HomePage
+    {
+        private IEnumerable<Trail>? _trails;
+
+        [Inject] public HttpClient Http { get; set; } = default!;
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                _trails = await Http.GetFromJsonAsync<IEnumerable<Trail>>("trails/trail-data.json");
+            }
+            catch(HttpRequestException ex)
+            {
+                Console.WriteLine($"There was a problem loading trail data: {ex.Message}");
+            }
+        }
+    }
+}
